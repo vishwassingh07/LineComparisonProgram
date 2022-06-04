674248133563272a524b619588c6ef5fc73c1054
Vishwas <baghel.vishwas0@gmail.com>
2022-06-04 23:15:32 +0530

    [Vishwas]add: Calculating The Length Of Line

---
+++ ComparisonOfLines/LineComparison.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisonOfLines
+{
+    public class LineComparison
+    {
+        int x1, x2, y1, y2;
+        public LineComparison(int x1, int x2, int y1, int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+        public void CalculatingLength()
+        {
+            double result = Math.Sqrt((x2 - x1)^2 + (y2 -y1)^2);
+            Console.WriteLine("Length Of The Line is {0} :" ,result);
+        }
+    }
+}
--- ComparisonOfLines/Program.cs
+++ ComparisonOfLines/Program.cs
@@ -6,6 +6,8 @@ namespace ComparisonOfLines
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Line Comparison Program.....");
+            LineComparison length = new LineComparison(2,5,4,9);
+            length.CalculatingLength();
         }
     }
 }
