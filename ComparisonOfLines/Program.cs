47631224598759401643e17fa2d0354570ab787f
Vishwas <baghel.vishwas0@gmail.com>
2022-06-05 09:42:23 +0530

    [Vishwas]add: Comparing The Two Lines

--- ComparisonOfLines/Program.cs
+++ ComparisonOfLines/Program.cs
@@ -9,18 +9,26 @@ namespace ComparisonOfLines
             //LineComparison length = new LineComparison(2,5,4,9);
             //length.CalculatingLength();
 
-            LineComparison equating = new LineComparison(1, 4, 6, 8);
+            LineComparison equating = new LineComparison(1, 4 ,2, 8);
             double line1 = equating.CalculatingLength();
             LineComparison equating2 = new LineComparison(2, 5, 3, 8);
             double line2 = equating2.CalculatingLength();
 
-            if (line1.Equals(line2))
+            if (line1.CompareTo(line2) == 0)
             {
                 Console.WriteLine("Both the Lines are equal");
             }
             else
             {
                 Console.WriteLine("Both the Lines are not equal");
+            }
+            if (line1.CompareTo(line2) > 0)
+            {
+                Console.WriteLine("Line 1 is greater than Line 2");
+            }
+            else
+            {
+                Console.WriteLine("Line 2 is greater than Line1");
             }
         }
     }
